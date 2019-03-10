6cf3bcd84450b41dbd99c2b95ee045d2f9f944f6
Joel <joelmaxw311@gmail.com>
2019-03-10 03:06:23 -0700

    Added a title screen

--- public_html/src/MyGame/MyGame.js
+++ public_html/src/MyGame/MyGame.js
@@ -13,12 +13,12 @@
 
 function MyGame() {
     this.kUIButton = "assets/UI/button.png";
+    this.kBackground = "assets/balloon_valhalla.png";
     
     // The camera to view the scene
     this.mCamera = null;
-    this.ParticleButton = null;
-    this.PhysicsButton = null;
-    this.UIButton = null;
+    this.PlayButton = null;
+    this.UIBackground = null;
     this.UIText = null;
     this.LevelSelect = null;
 }
@@ -27,18 +27,17 @@ gEngine.Core.inheritPrototype(MyGame, Scene);
 
 MyGame.prototype.loadScene = function () {
     gEngine.Textures.loadTexture(this.kUIButton);
+    gEngine.Textures.loadTexture(this.kBackground);
 };
 
 MyGame.prototype.unloadScene = function () {
     gEngine.Textures.unloadTexture(this.kUIButton);
-    if(this.LevelSelect==="Particle"){
-        gEngine.Core.startScene(new ParticleLevel());
-    }
-    else if(this.LevelSelect==="Physics"){
-        gEngine.Core.startScene(new RigidShapeDemo());
-    }
-    else if(this.LevelSelect==="UI"){
-        gEngine.Core.startScene(new UIDemo());
+    gEngine.Textures.unloadTexture(this.kBackground);
+    
+    if(this.LevelSelect==="Play"){
+        gEngine.Core.startScene(new MazeLevel());
+    } else {
+        gEngine.Core.startScene(new MyGame());
     }
 };
 
@@ -53,10 +52,15 @@ MyGame.prototype.initialize = function () {
             // sets the background to gray
     gEngine.DefaultResources.setGlobalAmbientIntensity(3);
     
-    this.ParticleButton = new UIButton(this.kUIButton,this.particleSelect,this,[400,400],[600,100],"Particle Demos",8,[1,1,1,1],[0,0,0,1]);
-    this.PhysicsButton = new UIButton(this.kUIButton,this.physicsSelect,this,[400,300],[500,100],"Physics Demo",8,[1,1,1,1],[0,0,0,1]);
-    this.UIButton =  new UIButton(this.kUIButton,this.uiSelect,this,[400,200],[320,100],"UI Demo",8,[1,1,1,1],[0,0,0,1]);
-    this.UIText = new UIText("Game Engine Tech Demo",[400,600],8,1,0,[0,0,0,1]);
+    this.PlayButton = new UIButton(this.kUIButton,this.particleSelect,this,[500,80],[600,100],"Play",8,[1,1,1,1],[0,0,0,1]);
+    
+    this.UIBackground = new TextureRenderable(this.kBackground);
+    this.UIBackground.getXform().setXPos(this.mCamera.getWCCenter()[0]);
+    this.UIBackground.getXform().setYPos(this.mCamera.getWCCenter()[1]);
+    this.UIBackground.getXform().setWidth(this.mCamera.getWCHeight() * 2);
+    this.UIBackground.getXform().setHeight(this.mCamera.getWCHeight());
+    
+    this.UIText = new UIText("Balloon Valhalla",[400,600],8,1,0,[0,0,0,1]);
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
@@ -66,29 +70,16 @@ MyGame.prototype.draw = function () {
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
     
     this.mCamera.setupViewProjection();
-    this.ParticleButton.draw(this.mCamera);
-    this.PhysicsButton.draw(this.mCamera);
-    this.UIButton.draw(this.mCamera);
+    this.UIBackground.draw(this.mCamera);
+    this.PlayButton.draw(this.mCamera);
     this.UIText.draw(this.mCamera);
 };
 
 MyGame.prototype.update = function () {
-    this.ParticleButton.update();
-    this.PhysicsButton.update();
-    this.UIButton.update();
+    this.PlayButton.update();
 };
 
 MyGame.prototype.particleSelect = function(){
-    this.LevelSelect="Particle";
-    gEngine.GameLoop.stop();
-};
-
-MyGame.prototype.physicsSelect = function(){
-    this.LevelSelect="Physics";
-    gEngine.GameLoop.stop();
-};
-
-MyGame.prototype.uiSelect= function(){
-    this.LevelSelect="UI";
+    this.LevelSelect="Play";
     gEngine.GameLoop.stop();
 };
