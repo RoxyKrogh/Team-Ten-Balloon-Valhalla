0b61338ff90d308f3e4aba0c239cf6f6e25af0f1
Sam Krogh <kroghs@uw.edu>
2019-03-10 03:01:40 -0700

    Fixed bugs in the lighting
    Fixed some bugs in the lighting part of the engine, including proper alpha handling for textured objects and proper light array size constant.

--- public_html/src/MyGame/Objects/Balloon.js
+++ public_html/src/MyGame/Objects/Balloon.js
@@ -6,7 +6,7 @@
  */
 
 /*jslint node: true, vars: true */
-/*global gEngine: false, GameObject: false, TextureRenderable: false */
+/*global gEngine: false, GameObject: false, TextureRenderable: false, vec2: false */
 /* find out more about jslint: http://www.jslint.com/help.html */
 
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
